616157d8d8c2a505ddf5839817baebfd525f01ee
Hft Root <hft@ci.cd>
2026-03-27 17:25:44 +0000

    Merge pull request #49 from kindkitchen/dev
    dev

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { User, UserId } from "./domain/auth/User";
+import { FormKit } from "@formkit/vue";
+
+const user = User({ id: UserId(crypto.randomUUID()) });
+</script>
+
+<template>
+  <FormKit type="button" label="Sign In with Google"></FormKit>
+</template>
